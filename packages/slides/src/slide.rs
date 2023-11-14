8e93cf8fcf6f3696d5f0e2321e7e7b29731f03bb
AJT <alextrelore@gmail.com>
2023-11-14 19:31:45 +0000

    minor refactoring, moving slide nav logic to container

--- packages/slides/src/slide.rs
+++ packages/slides/src/slide.rs
@@ -16,40 +16,12 @@ pub struct SlideProps<'a> {
 }
 
 pub fn Slide<'a, S: Slidable + Clone + 'static>(cx: Scope<'a, SlideProps<'a>>) -> Element<'a> {
-    let deck = use_shared_state::<S>(cx).expect("Failed to get shared state");
-
-    let prev = deck.read().prev();
-    let next = deck.read().next();
-
     cx.render(rsx! {
         div {
             style: "position: relative; min-height: 99vh; min-width: 99vw;",
             div {
                 style: "z-index: 0; height: 100%; width: 100%; position: absolute; top: 0; left: 0;",
                 cx.props.content.clone()
-            }
-            if let Some(prev) = prev.clone() {
-                render! {
-                    div {
-                        // show on the left side of the screen for 20% of the screen
-                        style: "z-index: 10; height: 100%; width: 20%; position: absolute; top: 0; left: 0;",
-                        onclick: move |_| {
-                            let mut deck = deck.write();
-                            *deck = prev.clone();
-                        }
-                    }
-                }
-            }
-            if let Some(next) = next.clone() {
-                render! {
-                    div {
-                        style: "z-index: 10; height: 100%; width: 20%; position: absolute; top: 0; left: 80%;",
-                        onclick: move |_| {
-                            let mut deck = deck.write();
-                            *deck = next.clone();
-                        }
-                    }
-                }
             }
         }
     })
--- packages/slides/src/slide_container.rs
+++ packages/slides/src/slide_container.rs
@@ -35,43 +35,66 @@ where
     <S as FromStr>::Err: std::fmt::Display,
     <S as FromStr>::Err: std::fmt::Debug,
 {
-    use_shared_state_provider(cx, || S::default());
-    let deck = use_shared_state::<S>(cx).expect("Failed to get shared state");
+    let deck = use_state::<S>(cx, || S::default());
+    let prev = deck.prev();
+    let next = deck.next();
+
+    let onkeydown = move |event: KeyboardEvent| {
+        if cx.props.enable_keyboard_navigation {
+            let a = match event.key() {
+                Key::ArrowRight => deck.next(),
+                Key::ArrowLeft => deck.prev(),
+                _ => None,
+            };
+            if let Some(a) = a {
+                deck.set(a)
+            }
+        }
+    };
 
     cx.render(rsx! {
         div {
             tabindex: "0",
-            onkeydown: |event: KeyboardEvent| {
-                if cx.props.enable_keyboard_navigation {
-                    let a = match event.key() {
-                        Key::ArrowRight => deck.write().next(),
-                        Key::ArrowLeft => deck.write().prev(),
-                        _ => None,
-                    };
-                    if let Some(a) = a {
-                        *deck.write() = a;
+            onkeydown: onkeydown,
+            style: "position: relative; min-height: 99vh; min-width: 99vw;",
+            div {
+                style: "z-index: 0; position: absolute; background-color: {cx.props.background_colour}; width: {cx.props.width}; height: {cx.props.height};",
+                deck.render(cx)
+            }
+            if let Some(prev) = prev.clone() {
+                render! {
+                    div {
+                        style: "z-index: 10; height: 100%; width: 20%; position: absolute; top: 0; left: 0;",
+                        onclick: move |_| {
+                            deck.set(prev.clone())
+                        }
                     }
                 }
-            },
-            style: "position: relative; min-height: 99vh; min-width: 99vw;",
+            }
+            if let Some(next) = next.clone() {
+                render! {
+                    div {
+                        style: "z-index: 10; height: 100%; width: 20%; position: absolute; top: 0; left: 80%;",
+                        onclick: move |_| {
+                            deck.set(next.clone())
+                        }
+                    }
+                }
+            }
             if cx.props.show_slide_progress_bar {
                 render! {
                     progress {
                         style: "z-index: 10; position: absolute; top: 0; left: 0; width: 100%;",
-                        max: "{deck.read().number_of_slides().to_string()}",
-                        value: "{deck.read().slide_number().to_string()}",
+                        max: "{deck.number_of_slides().to_string()}",
+                        value: "{deck.slide_number().to_string()}",
                     }
                 }
-            }
-            div {
-                style: "z-index: 0; position: absolute; background-color: {cx.props.background_colour}; width: {cx.props.width}; height: {cx.props.height};",
-                deck.read().render(cx)
-            }
+            },
             if cx.props.show_slide_no {
                 render! {
                     div {
                         style: "z-index: 10; position: absolute; bottom: 5%; right: 5%;",
-                        "Slide: {deck.read().slide_number().to_string()} of {deck.read().number_of_slides().to_string()}"
+                        "Slide: {deck.slide_number().to_string()} of {deck.number_of_slides().to_string()}"
                     }
                 }
             }
